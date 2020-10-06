de929fa9f30a07b954971925951a13e4e3481cce
Ovi <oviice@gmail.com>
2020-10-06 17:37:15 +0600

    notification

---
+++ app/src/main/java/com/example/notificationtest/CustomNotificationClass.java
@@ -0,0 +1,41 @@
+package com.example.notificationtest;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+public class CustomNotificationClass {
+    Context context;
+    String channelId;
+
+    public CustomNotificationClass(Context context, String CHANNEL_ID) {
+        this.context = context;
+        channelId = CHANNEL_ID;
+    }
+
+    void sendNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(context, channelId)
+                        .setSmallIcon(R.drawable.ic_launcher_background) //set icon for notification
+                        .setContentTitle("Notifications Example") //set title of notification
+                        .setContentText("This is a notification message")//this is notification message
+                        .setAutoCancel(true) // makes auto cancel of notification
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT); //set priority of notification
+
+
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //notification message will get at NotificationView
+        notificationIntent.putExtra("message", "This is a notification message");
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+}
--- app/src/main/java/com/example/notificationtest/MainActivity.java
+++ app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -13,10 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static com.example.notificationtest.MyBroadcastReceiver.ACTION_SNOOZE;
-import static com.example.notificationtest.MyBroadcastReceiver.EXTRA_NOTIFICATION_ID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,11 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         createNotificationChannel();
-
+        CustomNotificationClass customNotificationClass = new CustomNotificationClass(this, CHANNEL_ID);
         Log.e(TAG, "onCreate");
 
-        LongOperation lo = new LongOperation(this);
-        lo.execute("Test 1", "Test 2", "Test 3");
+        customNotificationClass.sendNotification();
     }
 
     private void createNotificationChannel() {
@@ -53,73 +48,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private class LongOperation extends AsyncTask<String, String, String> {
-
-        private static final String TAG = "longoperation";
-        private Context ctx;
-        private AtomicInteger notificationId = new AtomicInteger(0);
-
-        LongOperation(Context ctx) {
-            this.ctx = ctx;
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            for (String s : params) {
-                Log.e(TAG, s);
-
-                publishProgress(s);
-
-                for (int i = 0; i < 5; i++) {
-                    try {
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        Thread.interrupted();
-                    }
-                }
-            }
-            return "Executed";
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            for (String title: values) {
-                sendNotification(title, notificationId.incrementAndGet());
-            }
-        }
-
-        void sendNotification(String title, int notificationId) {
-
-            // Create an explicit intent for an Activity in your app
-        /* Intent intent = new Intent(ctx, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(ctx, 0, intent, 0); */
-
-            Intent snoozeIntent = new Intent(ctx, MyBroadcastReceiver.class);
-            snoozeIntent.setAction(ACTION_SNOOZE);
-            snoozeIntent.putExtra(EXTRA_NOTIFICATION_ID, notificationId);
-
-            Log.e(TAG, snoozeIntent.getExtras().toString());
-
-            Log.e(TAG, "snoozeIntent id: " + snoozeIntent.getIntExtra(EXTRA_NOTIFICATION_ID, -1));
-
-            PendingIntent snoozePendingIntent =
-                    PendingIntent.getBroadcast(ctx, notificationId, snoozeIntent, 0);
-
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(ctx, CHANNEL_ID)
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setContentTitle(String.format("%s (id %d)", title, notificationId))
-                    .setContentText("Much longer text that cannot fit one line...")
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                    .setAutoCancel(false)
-                    // Add the action button
-                    .addAction(R.drawable.ic_launcher_foreground, ctx.getString(R.string.snooze),
-                            snoozePendingIntent);
-
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(ctx);
-
-            // notificationId is a unique int for each notification that you must define
-            notificationManager.notify(notificationId, builder.build());
-        }
-    }
 }
